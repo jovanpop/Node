a0d98be66bb1421c6b426d096147891d9644c5ee
jovanpop <jovan.popovski@outlook.com>
2021-09-21 22:15:57 +0200

    homework5

--- Homework5/routes/index.js
+++ Homework5/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let names =["Aco","Irina","Matea"];
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', names: names});
+  res.render('index', { title: 'Express', people: names});
 });
+// router.get ("/countries", (req,res)=>{
+//   res.render ('countries', {countries: [{name:"USA",city: "Washington",population: 300},{name:"Macedonia",city:"Skopje",population:2},{name:"Germany",city:"Berlin",population:80}]});
+// }); 
 module.exports = router;
