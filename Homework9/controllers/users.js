3465043ffb6b5c54ef8fe7fa33e0fff0bad2a6bb
jovanpop <jovan.popovski@outlook.com>
2021-10-22 18:57:35 +0200

    Added jwt and bcrypt to password on login

--- Homework9/controllers/users.js
+++ Homework9/controllers/users.js
@@ -1,16 +1,53 @@
 const User = require ('../models/users');
+const bcrypt= require ('bcryptjs');
+const jwt= require('jsonwebtoken');
 
 module.exports= {
     postUser: async(req,res)=>{  
-        req.body.email= req.body.email.toLowerCase();
-        const user = await User.create(req.body);
-        res.send(user);
+        try {
+            let user = await User.findOne({email: req.body.email.toLowerCase()});
+            if (user){
+                throw new Error ("This email is taken");
+            };
+            req.body.email= req.body.email.toLowerCase();
+            req.body.password = bcrypt.hashSync(req.body.password);
+            user = await User.create(req.body);
+            res.send({
+                error: false,
+                message: "Created",
+                user: user
+            })
+        } 
+        catch(error){
+            res.send({
+                error: true,
+                message: error.message
+            })
+        };
     },
     postLogIn: async(req,res)=>{
-        if (await User.findOne({email: req.body.email.toLowerCase(), password: req.body.password})){
-            res.send("Logged in");
-        } else{
-            res.send("Incorect pw or email");
+        try {
+            user = await User.findOne({email: req.body.email.toLowerCase()});
+            if (user && bcrypt.compareSync(req.body.password, user.password)){
+                const payload = {
+                    id: user._id,
+                    email: user.email
+                }
+                const token = jwt.sign(payload,"asd",{expiresIn: "10s"})
+                res.send({
+                    error: false,
+                    message: "Logged in",
+                    token: token
+                })
+            }else{
+                throw new Error("Invalid credentials");
+            }
+        }
+        catch (error){
+            res.send({
+                error: true,
+                message: error.message
+            })
+            };
         }
     }
-}
